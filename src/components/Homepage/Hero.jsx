272e43a9729003a96f7534728bbcc71f9f32dcdc
sandy2108 <dhonisanjay116@gmail.com>
2023-05-23 18:25:42 +0530

    hero margin update

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -4,9 +4,9 @@ import {BsArrowDown} from "react-icons/bs";
 
 const Hero = () => {
   return (
-    <div className='w-full h-full my-5'>
+    <div className='w-full h-full my-8'>
         <div className='m-4'>
-          <div className='max-w-[1400px] rounded-3xl flex-wrap background h-[680px] m-auto p-4'>
+          <div className='max-w-[1500px] rounded-3xl flex-wrap background h-[680px] m-auto p-4'>
             <div className='mx-auto'>
                 <h1 className=' font-inter font-normal text-transparent sm:text-9xl  m-5 text-7xl leading-none text-gradient bg-gradient-to-r from-gray-900 to-white bg-clip-text' >Build Beyond </h1>
             </div>
