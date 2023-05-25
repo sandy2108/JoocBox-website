30060bc8133968eecb02508164e015074ccd926b
sandy2108 <dhonisanjay116@gmail.com>
2023-05-25 12:19:56 +0530

    removed comp

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import About from '@/components/Homepage/About'
 import Design from '@/components/Homepage/Design'
 import Security from '@/components/Homepage/Security'
 import Community from '@/components/Homepage/Community'
-import Loading from '@/components/Loading/Loading'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +21,7 @@ export default function Home() {
       <Design/>
       <Security/>
       <Community/>
-      <Loading/>
+      
    </>
     
   )
