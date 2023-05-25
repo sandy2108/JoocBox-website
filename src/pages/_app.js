74768a3d4494142a06dba48ce3a492652a6e6286
sandy2108 <dhonisanjay116@gmail.com>
2023-05-25 10:27:27 +0530

    added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import Footer from '@/components/Footer/Footer'
 import Navbar from '@/components/Navbar/Navbar'
 import '@/styles/globals.css'
-
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return(
     <>
+     <Head>
+          <title>JoocBox - FashionFusion</title>
+      </Head>
     <Navbar/>
     <Component {...pageProps} />
     <Footer/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 
 import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar/Navbar'
-import Footer from '@/components/Footer/Footer'
 import Hero from '@/components/Homepage/Hero'
 import Intro from '@/components/Homepage/Intro'
 import Cards from '@/components/Homepage/Cards'
@@ -15,7 +13,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <>
+   <> 
       <Hero/>
       <Intro/>
       <Cards/>
