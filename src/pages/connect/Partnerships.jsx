3bb9a1ce473b2372bb8960add0cae8d78b978437
sandy2108 <dhonisanjay116@gmail.com>
2023-05-27 22:09:34 +0530

    added the layout for connect pages

--- src/pages/connect/Partnerships.jsx
+++ src/pages/connect/Partnerships.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 const Partnerships = () => {
   return (
-    <div className='w-full h-screen bg-orange-200'>
-        <div className='max-w-[1240px] mx-auto p-4'>
-            <div className='flex items-center justify-center p-20'>
-                <p className='text-5xl'>Hello Frank! I need detailed information about this and then only I will build this page. </p>
-            </div>
+    <div className='w-full md:h-screen h-full'>
+       <div className='my-3 px-4'>
+        <div className=' max-w-[1500px] h-[680px] bg-[#eaf7ff] my-10 rounded-3xl mx-auto p-4'>
+            
 
         </div>
-
+       </div>
     </div>
   )
 }
