e8497b2e6b333ed9ff8e6a81ae96e34a40c375c6
sandy2108 <dhonisanjay116@gmail.com>
2023-06-16 10:23:29 +0530

    research committed

---
+++ src/components/Research/RsHead.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const RsHead = () => {
+  return (
+    <div className="w-full h-full">
+      <div className="max-w-[1240px] mx-auto p-4">
+        <div className="my-10 p-30">
+            <p className="text-[64px] leading-[60px] font-medium ">Next Generation
+Technology Built with Next Generation Research </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RsHead;
